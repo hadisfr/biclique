c2f149c732adaefd8ae7235333997e4dfff0dc58
h@di <hadi@hadisafari.ir>
2018-08-16 19:37:05 +0430

    port to python3

--- bicli_break.py
+++ bicli_break.py
@@ -1,15 +1,16 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 #
 # break the biclique file into single bicliques.
 
+import sys
+import os
 SUFFIX = 'biclique'
 
-import sys, os
 
 if __name__ == '__main__':
     if len(sys.argv) > 4 or len(sys.argv) < 2:
-        print "Break the biclique file into single bicliques."
-        sys.exit("Usage: " + os.path.basename(sys.argv[0]) + " [biclique file] [m-cutoff] [n-cutoff]")
+        print('Break the biclique file into single bicliques.')
+        sys.exit('Usage: ' + os.path.basename(sys.argv[0]) + ' [biclique file] [m-cutoff] [n-cutoff]')
 
     m, n = 0, 0
     if len(sys.argv) == 3:
@@ -19,18 +20,18 @@ if __name__ == '__main__':
     else:
         m, n = int(sys.argv[2]), int(sys.argv[3])
 
-    print 'Cut-off values: %d, %d' % (m, n)
-    print "Reading: %s." % sys.argv[1]
+    print('Cut-off values: %d, %d' % (m, n))
+    print('Reading: %s.' % sys.argv[1])
     src = open(sys.argv[1], 'r').readlines()
 
-    for i in xrange(0, len(src), 3):
-        #print "%d" % (i/3)
-        cliq_name = "%d.%s" % (i/3, SUFFIX)
+    for i in range(0, len(src), 3):
+        # print '%d' % (i/3)
+        cliq_name = '%d.%s' % (i / 3, SUFFIX)
         # applying the filter now.
-        if len(src[i].split()) >= m and len(src[i+1].split()) >= n:
+        if len(src[i].split()) >= m and len(src[i + 1].split()) >= n:
 
-            print "Writing: %s." % cliq_name
+            print('Writing: %s.' % cliq_name)
             f = open(cliq_name, 'w')
             f.write(src[i])
-            f.write(src[i+1])
+            f.write(src[i + 1])
             f.close()
